01e75516ac76741dba739cf92ac3ceef3eff858a
Luis Nieto <luisnieto@MBP-de-DSI.lan>
2024-06-15 10:32:23 +0200

    feat: updated assets

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,57 +2,57 @@
 module.exports = {
   important: true,
   darkMode: "selector",
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
     colors: {
-      'blue-primary': '#002347',
-      'blue-secondary': ' #003366',
-      'blue-tertiary': '#003f7d',
-      'orange-primary': '#ff8e00',
-      'orange-secondary': '#fd7702',
-      'orange-tertiary': '#ff5003',
-      'blue': '#1fb6ff',
-      'red': '#ff0000',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#fbfbfb',
+      "blue-primary": "#002347",
+      "blue-secondary": " #003366",
+      "blue-tertiary": "#003f7d",
+      "orange-primary": "#ff8e00",
+      "orange-secondary": "#fd7702",
+      "orange-tertiary": "#ff5003",
+      "blue": "#1fb6ff",
+      "red": "#ff0000",
+      "purple": "#7e5bef",
+      "pink": "#ff49db",
+      "orange": "#ff7849",
+      "green": "#13ce66",
+      "yellow": "#ffc82c",
+      "gray-dark": "#273444",
+      "gray": "#8492a6",
+      "gray-light": "#fbfbfb",
     },
     fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
     },
     screens: {
-      'sm': '640px',
+      "sm": "640px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      "md": "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      "lg": "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      "xl": "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
     },
     spacing: {
-      '0': '0px',
-      '1': '8px',
-      '2': '12px',
-      '3': '16px',
-      '4': '24px',
-      '5': '32px',
-      '6': '48px',
-      '7': '96px'
-    }
+      "0": "0px",
+      "1": "8px",
+      "2": "12px",
+      "3": "16px",
+      "4": "24px",
+      "5": "32px",
+      "6": "48px",
+      "7": "96px",
+    },
   },
   plugins: [],
-}
+};
